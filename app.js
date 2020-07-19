348b29a0924c383cab2cb01304ef998c7a9f4053
Adetutu Gbangbola <60041984+Adetutu777@users.noreply.github.com>
2020-07-19 21:29:49 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+// buttonOne Dom manipulation
+const buttonOne = document.getElementById('buttonOne'),
+ staffCheck = document.getElementById('classcheckOne').value,
+ passCheck = document.getElementById('classcheckTwo').value;
+
+
+ buttonOne.addEventListener('click', function(){
+
+
+ let click = {
+    Details: [
+      {
+          Name: 'jacob Kent',
+          Department: 'Admin',
+          staffIdNo : 300,
+          Password:1234,
+      },
+  
+      {
+          Name: 'Paul Simon',
+          Department: 'IT',
+          staffIdNo :113,
+          Password: 3112
+      },
+  
+      {
+          Name: 'Jessica Smith',
+          Department: 'Legal',
+          staffIdNo : 485,
+          Password: 5211
+      },
+  
+    ]}
+
+})
