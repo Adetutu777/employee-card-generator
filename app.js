89fb2b02619bf93490392f888568c8e4d9c73296
Adetutu Gbangbola <60041984+Adetutu777@users.noreply.github.com>
2020-07-24 21:00:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 // buttonOne Dom manipulation
-const buttonOne = document.getElementById('buttonOne'),
- staffCheck = document.getElementById('classcheckOne').value,
- passCheck = document.getElementById('classcheckTwo').value;
+// const buttonOne = document.getElementById('buttonOne'),
+//  staffCheck = document.getElementById('classcheckOne').value,
+//  passCheck = document.getElementById('classcheckTwo').value;
 
 
- buttonOne.addEventListener('click', function(){
+//  buttonOne.addEventListener('click', function(){
 
 
- let click = {
-    Details: [
-      {
-          Name: 'jacob Kent',
-          Department: 'Admin',
-          staffIdNo : 300,
-          Password:1234,
-      },
+//  let click = {
+//     Details: [
+//       {
+//           Name: 'jacob Kent',
+//           Department: 'Admin',
+//           staffIdNo : 300,
+//           Password:1234,
+//       },
   
-      {
-          Name: 'Paul Simon',
-          Department: 'IT',
-          staffIdNo :113,
-          Password: 3112
-      },
+//       {
+//           Name: 'Paul Simon',
+//           Department: 'IT',
+//           staffIdNo :113,
+//           Password: 3112
+//       },
   
-      {
-          Name: 'Jessica Smith',
-          Department: 'Legal',
-          staffIdNo : 485,
-          Password: 5211
-      },
+//       {
+//           Name: 'Jessica Smith',
+//           Department: 'Legal',
+//           staffIdNo : 485,
+//           Password: 5211
+//       },
   
-    ]}
+//     ]}
 
-})
+// })
