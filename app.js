f1a875fe61d22c897fd3099536ec5792d4e32d44
Adetutu Gbangbola <60041984+Adetutu777@users.noreply.github.com>
2020-07-27 19:19:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,50 @@
+
+ let users =  [
+      {
+          username: 'jacobkent',
+          department: 'Admin',
+          staffIdNo : 300,
+          password:1234,
+      },
+  
+      {
+          username: 'paulsimon',
+          department: 'IT',
+          staffIdNo :113,
+          password: 3112
+      },
+  
+      {
+          username: 'jessicasmith',
+          department: 'Legal',
+          staffIdNo : 485,
+          password: 5211
+      }
+    ]
+
 // buttonOne Dom manipulation
-// const buttonOne = document.getElementById('buttonOne'),
-//  staffCheck = document.getElementById('classcheckOne').value,
-//  passCheck = document.getElementById('classcheckTwo').value;
+let buttonOne = document.getElementById('button-one');
+let staff = document.getElementById('class-check-one');
+let pass = document.getElementById('class-check-two');
+let errrorOutput = document.getElementById('error-text');
 
+   let valRecords =()=> {
+      let nameVal = staff.value;
+      let passVal = pass.value;
+   	const checkname = users.some(person => person.username == nameVal)
+   	const checkpass = users.some(person => person.password == passVal)
+   	staff.value ='';
+     pass.value='';
 
-//  buttonOne.addEventListener('click', function(){
+   	if (checkname && checkpass ===true){
+   		console.log('right records')
+   		location.assign("./dashboard.html");
+   	}
 
+   	else{
+   		errrorOutput.innerHTML ='Wrong Username/Password';
+   	}
 
-//  let click = {
-//     Details: [
-//       {
-//           Name: 'jacob Kent',
-//           Department: 'Admin',
-//           staffIdNo : 300,
-//           Password:1234,
-//       },
-  
-//       {
-//           Name: 'Paul Simon',
-//           Department: 'IT',
-//           staffIdNo :113,
-//           Password: 3112
-//       },
-  
-//       {
-//           Name: 'Jessica Smith',
-//           Department: 'Legal',
-//           staffIdNo : 485,
-//           Password: 5211
-//       },
-  
-//     ]}
+      }
 
-// })
+buttonOne.addEventListener('click', valRecords)
